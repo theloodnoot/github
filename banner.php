ac935aa6321c76f4d574bb01e9e27cc43caf6a06
thelordnoot <93921972+theloodnoot@users.noreply.github.com>
2022-06-20 15:06:34 +0100

    Add files via upload

---
+++ banner.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Prototype</title>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  </head>
+
+	<header>
+			<nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+				<div class="container">
+
+					<div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
+						<ul class="navbar-nav flex-grow-1">
+							<li class="nav-item">
+								<?php //<a// class="nav-link text-dark" href="/attempt 2/admin.php">Login for Admin</a> ?>
+							</li>
+							<li class="nav-item">
+                                 <a class="nav-link text-dark">movie movement system</a>
+                            </li>
+						</ul>
+					</div>
+				</div> 
+			</nav>
+	</header>
